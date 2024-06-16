92c10038bcc27b6abdf76f5212883e1e10daa5b9
ahogg <aidan@aidanhogg.co.uk>
2024-06-16 20:12:19 +0100

    Changed hrtf_size to 16

--- main.py
+++ main.py
@@ -83,8 +83,8 @@ def main(config, mode):
 
         # collect all train_hrtfs to get mean and sd
         train_hrtfs = []
-        # for i in range(len(ds)):
-        for i in range(20):
+        for i in range(len(ds)):
+        # for i in range(20):
             if i % 10 == 0:
                 print(f"HRTF {i} out of {len(ds)} ({round(100 * i / len(ds))}%)")
 
